e92d3dfb0ccea9bc328c787b9bcf86aa521a14d6
NamiraAidaHairunnisa <namida124@gmail.com>
2023-05-09 11:27:31 +0800

    merapikan about us page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 let aboutTeks = document.getElementById('aboutteks');
 let kursi = document.getElementById('kursi');
-// let stars = document.getElementById('stars');
-
 
 window.addEventListener('scroll', () => {
     let value = Math.min(Math.max(window.scrollY, 0), 350);
 
-    aboutTeks.style.top = value * 2 + 'px';
-    kursi.style.top = value * 1 + 'px';
-    // stars.style.top = value * 1 + 'px';
+    aboutTeks.style.top = (value * 3 - 400) + 'px'; 
+    kursi.style.top = (value * 1 - 100) + 'px'; 
 });
