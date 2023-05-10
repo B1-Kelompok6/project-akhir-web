91045962343fc593cd4d84bc76b1b730ff3ab2fa
unknown <ryanndapn@gmail.com>
2023-05-10 09:20:39 +0800

    menambahkan detail halaman pada page now playing dan up coming

--- detail_page1.php
+++ detail_page1.php
@@ -22,3 +22,179 @@
             </ul>
         </nav>
     </div>
+
+<head>
+	<title>Detail Film</title>
+	<style>
+		body {
+			background-color: #f5f5f5	;
+			font-synthesis: small-caps;
+			padding: 10;
+		}
+
+		head {
+			margin-bottom: 50px;
+
+		}
+
+		h1 {
+			font-size: 24px;
+ 			margin-bottom: 10px;
+		
+			
+			color:#000000	;
+			
+			
+		}
+
+		main {
+			max-width: 1000px;
+			margin: 10;
+			padding: 30px;
+		}
+
+		section {
+			margin-bottom: 10px;
+		}
+
+		h2 {
+			font-size: 26px;
+			line-height: 1.0;
+			margin-bottom: 10px;
+			color:#000000	;
+		}
+		.button-link {
+			
+			margin-top: 15px;
+			
+			display: inline-block;
+			padding: 10px 10px;
+			background-color: #4CAF50;
+			color: white;
+			font-size: 18px;
+			border-radius: 10px;
+			text-decoration: none;
+		}
+		.button-link2 {
+			
+			margin-bottom: 0px;
+			display: inline-block;
+			padding: 10px 20px;
+			background-color: #4CAF50;
+			color: white;
+			font-size: 18px;
+			border-radius: 10px;
+			text-decoration: none;
+		}
+		.review-button {
+		
+			display: inline-block;
+			padding: 10px 30px;
+			background-color: #4CAF50;
+			color: white;
+			font-size: 18px;
+			border-radius: 10px;
+			text-decoration: none;
+		}
+		.age-rating-button {
+			float:right;
+			display: inline-block;
+			padding: 12px;
+			background-color: rgb(21, 21, 46);
+			color: white;
+			font-size: 18px;
+			border-radius: 50%;
+			border: none;
+			cursor: pointer;
+			text-align: center;
+			text-decoration: none;
+			}
+
+		img {
+			display: block;
+			margin: 10 auto;
+			max-width: 1000%;
+			height: auto;
+		}
+
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+			color:#000000	;
+		}
+
+		li {
+			margin-bottom: 10px;
+		}
+
+		.float-left {
+		float: left;
+		margin-right: 30px; /* Jarak antara gambar dan teks */
+		}
+
+		.float-right {
+		float: right;
+		margin-left: 30px; /* Jarak antara teks dan gambar */
+		}
+
+
+	</style>
+
+
+	<head>
+		<h1></h1>
+		
+	</head>
+	<main>
+		<section>
+		
+    		<section>
+				<h2>
+				Now Playing
+				<button class="age-rating-button">13+</button>
+
+				</h2>
+				<img src="https://media.21cineplex.com/webcontent/gallery/pictures/168017501031489_290x426.jpg" alt="Poster Film" class="float-left">
+			
+			</section>
+			<section>
+				<h2>
+					Guardians Of Galaxy Vol.3
+				</h2>
+	</section>
+		<section>
+	
+			
+		
+		<ul>
+		<li>Film ketiga akan melanjutkan kisah petualangan Peter Quill/Star Lord (Chris Pratt) dan teman-temannya sebagai penjaga galaksi.Masih berduka karena kehilangan Gamora (Zoe Saldana), Peter mengumpulkan timnya untuk kembali melindungi alam semesta. Misi kali ini tidaklah mudah, jika tidak berhasil, maka akan menjadi akhir bagi para Guardians.</li>
+		</ul>
+		
+		</section>
+        <section>
+
+			<h2>Detail</h2>
+			<ul>
+				<li>Jadwal Tayang: 2023-05-20</li>
+				<li>waktu        : 19:30:00	</li>
+				<li>Durasi       : 92 menit</li>
+				<li>Studio       : 2D<li>
+				
+				
+				
+			</ul>
+			<ul>
+				
+			<li><a href="https://youtu.be/u3V5KDHRQvk" class = "button-link "> Watch Trailer</a></li>
+			<li><a href="" class ="button-link2">Buy Ticket</a><li>
+			<li><a href="" class ="review-button">Review</a><li>
+				<div class="video-link float-left">
+			</ul>
+		</section>
+		
+	
+		
+	</main>
+
+</html>
