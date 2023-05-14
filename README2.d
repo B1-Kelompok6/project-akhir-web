b1f6f5ef29942d3ec47ec4db3182251fe697df35
Ryanda Putra Nasrullah <118449655+YandaPN@users.noreply.github.com>
2023-05-14 10:19:21 +0800

    Create README2.md

---
+++ README2.d
@@ -0,0 +1,6 @@
+Project Akhir praktikum web
+Untuk kelompok B1-6 yang terdiri dari
+- Eggy Brilian
+- Namira Aida 
+- Ryanda Putra
+Membuat sebuah website yang bertemakan tiket bioskop berjudul CINEMA KW
